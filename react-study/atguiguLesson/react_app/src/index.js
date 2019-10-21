dd0060c589cbed82e4056bb0c0a427ab3f16f93f
backtobe <ZhangLin_SC@163.com>
2019-10-21 19:15:30 +0800

    Today code

--- react-study/atguiguLesson/react_app/src/index.js
+++ react-study/atguiguLesson/react_app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/app.jsx';
+//必须相对路径
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
